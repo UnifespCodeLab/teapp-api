0cce51df47c35ff7aacc845f473f1c9d4f69eac2
Marcos Magueta <maguetamarcos@gmail.com>
2020-09-11 20:15:24 -0300

    Usuarios complementados e vizinhancas adicionadas

--- app.py
+++ app.py
@@ -15,14 +15,30 @@ class Usuario(db.Model):
     password = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     verificado = db.Column(db.Boolean, default=False, nullable=False)
+    sexo = db.Column(db.String(1), nullable=False)
+    nascimento = db.Column(db.String(20), nullable=False)
+    cor = db.Column(db.String(10), nullable=False)
+    telefone = db.Column(db.String(20), nullable=False)
+    rua = db.Column(db.String(100), nullable=False)
+    numero_casa = db.Column(db.Integer, nullable=False)
+    data_registro = db.Column(db.DateTime, nullable=False)
+    bairro = db.Column(db.Integer, db.ForeignKey('bairros.id'), nullable=False)
     user_type = db.Column(db.Integer, db.ForeignKey('privilegios.id'), nullable=False)
 
-    def __init__(self, real_name, password, email, user_type):
+    def __init__(self, real_name, password, email, user_type, sexo, nascimento, cor, ):
+        import datetime
         self.real_name = real_name
         self.password = password
         self.verificado = False
         self.email = email
         self.user_type = user_type
+        self.sexo = sexo
+        self.nascimento = nascimento
+        self.cor = cor
+        self.telefone = telefone
+        self.rua = rua
+        self.numero_casa = numero_casa
+        self.data_registro = datetime.now()
 
 
 class Privilegio(db.Model):
@@ -32,6 +48,14 @@ class Privilegio(db.Model):
 
     def __init__(self, user_type):
         self.user_type = user_type
+
+class Bairro(db.Model):
+    __tablename__ = 'bairros'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __init__(self, nome):
+        self.nome = nome
 
 class Postagem(db.Model):
     __tablename__ = 'postagens'
@@ -48,6 +72,13 @@ class Comentario(db.Model):
     postagem = db.Column(db.Integer, db.ForeignKey('postagens.id'), nullable=False)
     resposta = db.Column(db.Integer, db.ForeignKey('comentarios.id'), nullable=True)
 
+class Form_Socioeconomico(db.Model):
+    __tablename__ = 'form_socioeconomico'
+    id = db.Column(db.Integer, primary_key=True)
+    nome_rep_familia = db.Column(db.String(100), unique=True, nullable=False)
+    idade = db.Column(db.String(100), unique=True, nullable=False)
+    def __init__(self, user_type):
+        self.user_type = user_type
 
 @app.route('/')
 def hello():
@@ -58,7 +89,7 @@ def users():
     if request.method == 'POST':
         if request.is_json:
             data = request.get_json()
-            new_user = Usuario(real_name=data['real_name'], password=data['password'], email=data['email'], user_type=data['user_type'])
+            new_user = Usuario(real_name=data['real_name'], password=data['password'], email=data['email'], user_type=data['user_type'], bairro=data['bairro'])
             db.session.add(new_user)
             db.session.commit()
 
@@ -115,6 +146,29 @@ def privileges():
 
         return {"count": len(results), "Privileges": results, "message": "success"}
 
+@app.route('/bairros', methods=['POST', 'GET'])
+def bairros():
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            new_bairro = Bairro(nome=data['nome'])
+
+            db.session.add(new_bairro)
+            db.session.commit()
+
+            return {"message": f"Privilégio criado com sucesso"}
+        else:
+            return {"error": "A requisição não foi feita no formato esperado"}
+
+    elif request.method == 'GET':
+        bairros = Bairro.query.all()
+        results = [
+            {
+                "nome": bairro.nome
+            } for bairro in bairros]
+
+        return {"count": len(results), "Bairros": results, "message": "success"}
+
 @app.route('/users/<id>', methods=['GET', 'PUT', 'DELETE'])
 def handle_user(id):
     user = Usuario.query.get_or_404(id)
@@ -128,9 +182,16 @@ def handle_user(id):
 
     elif request.method == 'PUT':
         data = request.get_json()
-        user.email = data['email']
-        user.real_name = data['real_name']
+        #user.email = data['email']
+        #user.real_name = data['real_name']
         user.password = data['password']
+        user.verificado = True
+        user.sexo = data['sexo']
+        user.nascimento = data['nascimento']
+        user.cor = data['cor']
+        user.telefone = data['telefone']
+        user.rua = data['rua']
+        user.numero_casa = data['numero_casa']
 
         db.session.add(user)
         db.session.commit()
