000a1d9e2aa1a13bc4709966850112126fb28734
Danilo Alexandre <danilo.salexan@gmail.com>
2022-04-11 18:52:07 -0300

    python version

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import sys
+
 import os
 from flask_cors import cross_origin
 from api import app, PORTAL_NAME, VERSION
@@ -7,7 +9,7 @@ from api import controller
 # TODO: Mover para controller
 @app.route('/status')
 def hello():
-    return f'{PORTAL_NAME} API v{VERSION} Works! [{os.environ.get("ENV", "DEV")}]'
+    return f'{PORTAL_NAME} API v{VERSION} Works! [{os.environ.get("ENV", "DEV")}]\nPython: {sys.version_info}'
 
 
 if __name__ == '__main__':
