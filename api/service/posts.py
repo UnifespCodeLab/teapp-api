28c3cd50d4e73057e1197b0943ff0ea419a242be
maegima <andre.maegima@unifesp.br>
2022-04-18 13:23:36 -0300

    Fix posts delete

--- api/service/posts.py
+++ api/service/posts.py
@@ -77,10 +77,10 @@ def UpdateStamp(id, status, updater):
 
 
 def Remove(id, remover):
-    postagem = Comentario.query.get_or_404(id)
+    postagem = Postagem.query.get_or_404(id)
 
     if remover.id == postagem.created_user or VerifyAccess(remover, [ADMINISTRADOR, MODERADOR]):
-        comments = Comentario.query.filter_by(postagem=id).order_by(Comentario.data.desc()).all()
+        comments = Comentario.query.filter_by(postagem=id).order_by(Comentario.created_date.desc()).all()
         for comment in comments:
             db.session.delete(comment)
 
